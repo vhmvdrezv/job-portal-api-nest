95aa572a6005b0c6c134fac8caa54dbbad9f5a91
vhmvdrezv <ahmdrzashn@gmail.com>
2025-06-08 14:08:35 +0330

    Stop tracking prisma/migrations directory

--- prisma/migrations/20250528124734_adding_user_id_to_job/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Job" ADD COLUMN     "userId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "Job" ADD CONSTRAINT "Job_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
